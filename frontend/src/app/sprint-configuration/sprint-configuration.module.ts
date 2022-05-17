6f57b727cd7dd4a593ef7a343e7768bd7f9da207
FusedBaggins <faguiar408@gmail.com>
2022-05-17 19:58:00 -0300

    feat: add modules

---
+++ frontend/src/app/sprint-configuration/sprint-configuration.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SprintConfigurationRoutingModule } from './sprint-configuration-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SprintConfigurationRoutingModule
+  ]
+})
+export class SprintConfigurationModule { }
---
+++ frontend/src/app/squad/squad.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SquadRoutingModule } from './squad-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SquadRoutingModule
+  ]
+})
+export class SquadModule { }
