41358645df34e0b7a28f9c9f85a4ce940bbfd40e
FusedBaggins <faguiar408@gmail.com>
2022-05-19 20:10:31 -0300

    refactor: remove .dotenv values from config

--- backend/app/database/config.ts
+++ backend/app/database/config.ts
@@ -2,10 +2,11 @@ import { Options } from "sequelize";
 
 let config: Options = {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: undefined,
+    username: undefined,
+    password: undefined,
+    database: undefined,
+    logging: true,
     port: 5432,
     define: {
         timestamps: true,
