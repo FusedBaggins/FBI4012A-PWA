80bc765ed09487d1eddc8d661609a5a46ba59542
FusedBaggins <faguiar408@gmail.com>
2022-05-19 21:02:30 -0300

    feat: configure Cors Headers

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import cors from 'cors';
 import express from "express";
 import bodyParser from 'body-parser';
 
@@ -24,6 +25,9 @@ class Application {
     }
 
     private _setMiddlewares(): void {
+        this.express.use(cors({
+            origin: ['http://localhost:4200'],
+        }));
         this.express.use(express.json());
         this.express.use(bodyParser.urlencoded({ extended: true }));
     }
