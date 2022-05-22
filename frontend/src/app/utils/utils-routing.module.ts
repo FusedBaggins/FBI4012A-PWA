c22126392e6cc0eb97bb8b349e83fd4dea076213
FusedBaggins <faguiar408@gmail.com>
2022-05-22 20:16:25 -0300

    feat: add 404 route component

--- frontend/src/app/utils/utils-routing.module.ts
+++ frontend/src/app/utils/utils-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { HomeComponent } from './components/home/home.component';
+import { RouteNotFoundComponent } from './components/route-not-found/route-not-found.component';
 
 const routes: Routes = [
+  { path: 'authentication', component: AuthenticationComponent },
+  { path: '404', component:RouteNotFoundComponent},
   { path: '', component: HomeComponent },
-  { path: 'authentication', component: AuthenticationComponent }
+  { path: '**', redirectTo:'404', pathMatch:'full'}
 ];
 
 @NgModule({
--- frontend/src/app/utils/utils.module.ts
+++ frontend/src/app/utils/utils.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UtilsRoutingModule } from './utils-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { RouteNotFoundComponent } from './components/route-not-found/route-not-found.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AuthenticationComponent
+    AuthenticationComponent,
+    RouteNotFoundComponent
   ],
   imports: [
     CommonModule,
