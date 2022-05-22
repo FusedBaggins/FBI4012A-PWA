191541df0115233c03d485812e380cf50abe14db
FusedBaggins <faguiar408@gmail.com>
2022-05-22 20:10:27 -0300

    feat: update project routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('src/app/main/main.module').then(module => module.MainModule) }
+  { path: 'user', loadChildren: () => import('src/app/utils/utils.module').then(module => module.UtilsModule) },
+  { path: 'squad', loadChildren: () => import('src/app/squad/squad.module').then(module => module.SquadModule) },
+  { path: 'sprint', loadChildren: () => import('src/app/sprint/sprint.module').then(module => module.SprintModule) },
+  { path: 'settings', loadChildren: () => import('src/app/sprint-configuration/sprint-configuration.module').then(module => module.SprintConfigurationModule) },
+  { path: '', loadChildren: () => import('src/app/utils/utils.module').then(module => module.UtilsModule) },
 ];
 
 @NgModule({
--- frontend/src/app/utils/utils-routing.module.ts
+++ frontend/src/app/utils/utils-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'authentication', component: AuthenticationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
