5747cbc87107bf27c62c33fc00b3c7747ad04373
FusedBaggins <faguiar408@gmail.com>
2022-06-02 20:21:09 -0300

    fix: add missing route on sprint module

--- frontend/src/app/sprint/sprint-routing.module.ts
+++ frontend/src/app/sprint/sprint-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SprintListComponent } from './components/sprint-list/sprint-list.component';
+import { SprintDetailComponent } from './components/sprint-detail/sprint-detail.component';
 
 const routes: Routes = [
+  { path: ':id', component: SprintDetailComponent },
   { path: '', component: SprintListComponent }
 ];
 
