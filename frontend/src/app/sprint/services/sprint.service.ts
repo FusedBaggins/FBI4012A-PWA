1566dd99dcb8c3fc2be1b74ba76824b551a26633
FusedBaggins <faguiar408@gmail.com>
2022-06-18 15:21:38 -0300

    feat: add crud methods in sprint service

--- frontend/src/app/sprint/services/sprint.service.ts
+++ frontend/src/app/sprint/services/sprint.service.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 // Local
 import { SprintFilter } from '../interfaces/sprint-filter';
 import { environment } from 'src/environments/environment';
+import { Sprint } from '../interfaces/sprint';
 
 @Injectable({
   providedIn: 'root'
@@ -14,27 +15,33 @@ import { environment } from 'src/environments/environment';
 export class SprintService implements OnDestroy {
 
   private _errors$: BehaviorSubject<any>;
-  private _sprints$: BehaviorSubject<any>;
+  private _sprints$: BehaviorSubject<Sprint[]>;
   private _sprintsSubscription$: Subject<void>;
 
   constructor(private _http: HttpClient) {
     this._errors$ = new BehaviorSubject(undefined);
-    this._sprints$ = new BehaviorSubject([]);
     this._sprintsSubscription$ = new Subject<void>();
+    this._sprints$ = new BehaviorSubject<Sprint[]>([]);
   }
 
   requestSprints(filter: SprintFilter): void {
     this._sprintsSubscription$.next();
-    this._http.get(`${environment.apiEndpoint}/sprint/`).subscribe(
-      (sprints) => {
-        this._sprints$.next(sprints);
-      },
-      (error: HttpErrorResponse) => {
-        this._errors$.next(error)
+    this._http.get<Sprint[]>(`${environment.apiEndpoint}/sprint/`).subscribe(
+      {
+        next: (value: Sprint[]) => this._sprints$.next(value),
+        error: (error: HttpErrorResponse) => this._errors$.next(error),
       });
   }
 
-  getSprints(): Observable<any> {
+  postSprint(obj: Sprint): Observable<any> {
+    return this._http.post<Sprint>(`${environment.apiEndpoint}/sprint/`, obj)
+  }
+
+  patchSprint(id: number, obj: Sprint): Observable<any> {
+    return this._http.patch<Sprint>(`${environment.apiEndpoint}/sprint/${id}/`, obj)
+  }
+
+  getSprints(): Observable<Sprint[]> {
     return this._sprints$.asObservable();
   }
 
