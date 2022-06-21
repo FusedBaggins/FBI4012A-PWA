fdf51c2344d2fbef957160b9acbbf87c96c978a4
FusedBaggins <faguiar408@gmail.com>
2022-06-21 20:24:42 -0300

    feat: add status attribute

--- backend/app/models/sprint.ts
+++ backend/app/models/sprint.ts
@@ -4,6 +4,7 @@ import database from '../database/database';
 class Sprint extends Model {
     id!: number;
     name!: string;
+    status!: "Em andamento" | "Não iniciada" | "Encerrada";
     isActive!: boolean;
     startDate!: Date;
     endDate!: Date;
@@ -12,11 +13,19 @@ class Sprint extends Model {
 Sprint.init(
     {
         name: Sequelize.STRING,
+        status: Sequelize.STRING,
         isActive: Sequelize.BOOLEAN,
         startDate: Sequelize.DATE,
         endDate: Sequelize.DATE
     },
     { sequelize: database.connection, freezeTableName: true }
 );
+
+Sprint.addHook('beforeCreate', async (sprint: Sprint) => {
+    const now: Date = new Date();
+    if (now.getTime() > sprint.startDate.getTime()) sprint.status = 'Em andamento';
+    if (now.getTime() < sprint.startDate.getTime()) sprint.status = 'Não iniciada';
+    if (now.getTime() > sprint.endDate.getTime()) sprint.status = 'Encerrada';
+});
 
 export default Sprint;
